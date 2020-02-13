8c27b60029b2b069c0fe400f58b3d02cb187d946
ajaysolution <ajay.vandra.sa@gmail.com>
2020-02-13 12:37:09 +0530

    Initial Commit

---
+++ photoEdit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoEdit
+//
+//  Created by Ajay Vandra on 2/13/20.
+//  Copyright © 2020 Ajay Vandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
