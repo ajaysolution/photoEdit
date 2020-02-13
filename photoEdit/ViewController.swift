205428fcb9b7e72ee12d53749f3f656e2c0a52bb
ajaysolution <ajay.vandra.sa@gmail.com>
2020-02-13 14:01:14 +0530

    photoEdit create

--- photoEdit/ViewController.swift
+++ photoEdit/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIImagePickerControllerDelegate ,UINavigationControllerDelegate{
 
+    @IBOutlet weak var imgView: UIImageView!
+    
+    var context : CIContext!
+    var currentFilter : CIFilter!
+    
+    var currentImage : UIImage!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CIPhotoEffectNoir")
     }
 
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            return
+        }
+        dismiss(animated: true)
+        currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+       selectedImage()
+    }
+    @IBAction func filterButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        //image(imgView.image!, didFinishSavingWithError: nil, contextInfo : )
+    }
+    func selectedImage(){
+        if let cgImage = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent){
+            let processedImage = UIImage(cgImage: cgImage)
+            imgView.image = processedImage
+        }
+    }
+    @objc func image(_ image:UIImage, didFinishSavingWithError error : Error?,contextInfo : UnsafeRawPointer){
+        if let error = error{
+            let ac = UIAlertController(title: "save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "ok", style: .default))}
+            else{
+                let ac = UIAlertController(title: "saved", message: "image is saved", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "ok", style: .default))
+                present(ac,animated: true)
+            }
+        }
+    }
 
-}
 
